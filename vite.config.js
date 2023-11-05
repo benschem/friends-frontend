0484b2c645a8c252f80aa7f4680b4dc68709896d
Ben Schembri <benschembri@gmail.com>
2023-11-05 20:49:01 +1100

    added linting options for solid

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,35 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 // import devtools from 'solid-devtools/vite';
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
   plugins: [
-    /* 
+    /*
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
     // devtools(),
     solidPlugin(),
+    {
+      // default settings on build (i.e. fail on error)
+      ...eslint(),
+      apply: "build",
+    },
+    {
+      // do not fail on serve (i.e. local development)
+      ...eslint({
+        failOnWarning: false,
+        failOnError: false,
+      }),
+      apply: "serve",
+      enforce: "post",
+    },
   ],
   server: {
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
